a2af383884b7e3f379f76ebfa895e536b5a9f6a9
Domenik Töfflinger <domenikt96@yahoo.de>
2022-02-09 16:14:14 +0100

    Update main.js

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,79 +1,9 @@
 const path = require('path')
 const toPath = _path => path.join(process.cwd(), _path)
 
-const EXLUDED_DOCGEN_PROPS = [
-  'as',
-  'gap',
-  'sx',
-  '_hover',
-  '_active',
-  '_focus',
-  '_disabled',
-  'uppercase',
-  'zIndex',
-  'minSize',
-  'maxSize',
-  'boxShadow',
-  'textShadow',
-  'fontSize',
-  'letterSpacing',
-  'fontWeight',
-  'fontFamily',
-  'lineHeight',
-  'css',
-  'disableFocusStyles',
-  '_variants',
-  '_selected',
-  'bgGradient',
-  'backgroundGradient',
-  'bgClip',
-  'backgroundClip',
-  'focusBorderColor',
-  'align',
-  'justify',
-  'wrap',
-  'direction',
-  'basis',
-  'grow',
-  'shrink',
-  'spacing',
-  'transform',
-  'transformOrigin',
-  'enableGpuAcceleration',
-  'scale',
-  'scaleX',
-  'scaleY',
-  'rotate',
-  'translateX',
-  'translateY',
-  'skewY',
-  'skewX',
-  'ref',
-  '_invalid',
-  'invalidFocusBorderColor',
-]
-
-const ALLOWED_DOCGEN_NODE_MODULES = ['tippy.js']
-
+// seems there is a current issues with webpack, storybook and Chakra deps
+// https://github.com/chakra-ui/chakra-ui/issues/2527#issuecomment-810293915
 module.exports = {
-  core: {
-    builder: 'webpack5',
-  },
-  webpackFinal: async config => {
-    return {
-      ...config,
-      resolve: {
-        ...config.resolve,
-        alias: {
-          ...config.resolve.alias,
-          '@emotion/core': toPath('node_modules/@emotion/react'),
-          '@emotion/styled-base': toPath('node_modules/@emotion/styled'),
-          '@emotion/styled': toPath('node_modules/@emotion/styled'),
-          'emotion-theming': toPath('node_modules/@emotion/react'),
-        },
-      },
-    }
-  },
   stories: ['../src/**/*.stories.@(ts|tsx|js|jsx|mdx)'],
   addons: [
     '@storybook/addon-viewport',
@@ -88,36 +18,17 @@ module.exports = {
     '@storybook/addon-essentials',
     'storybook-dark-mode',
   ],
-  typescript: {
-    check: true,
-    reactDocgen: 'react-docgen-typescript',
-    reactDocgenTypescriptOptions: {
-      shouldExtractLiteralValuesFromEnum: true,
-      propFilter: (prop, component) => {
-        if (component.name === 'Box') {
-          return false
-        }
-        if (EXLUDED_DOCGEN_PROPS.includes(prop.name)) {
-          return false
-        }
-        if (
-          prop.parent &&
-          prop.parent.fileName &&
-          ALLOWED_DOCGEN_NODE_MODULES.some(module => prop.parent.fileName.includes(module))
-        ) {
-          return true
-        }
-        if (prop.parent && /node_modules/.test(prop.parent.fileName)) {
-          return false
-        }
-
-        return true
+  webpackFinal: async (config) => {
+    return {
+      ...config,
+      resolve: {
+        ...config.resolve,
+        alias: {
+          ...config.resolve.alias,
+          '@emotion/core': toPath('node_modules/@emotion/react'),
+          'emotion-theming': toPath('node_modules/@emotion/react'),
+        },
       },
-
-      compilerOptions: {
-        allowSyntheticDefaultImports: false,
-        esModuleInterop: false,
-      },
-    },
+    };
   },
 }

fcc47a1df6319bc689dd9df9aec43ed3f89b23d3
Domenik Töfflinger <domenikt96@yahoo.de>
2022-02-15 17:05:33 +0100

    Update main.js

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,66 +1,9 @@
 const path = require('path')
 const toPath = _path => path.join(process.cwd(), _path)
 
-const EXLUDED_DOCGEN_PROPS = [
-  'as',
-  'gap',
-  'sx',
-  '_hover',
-  '_active',
-  '_focus',
-  '_disabled',
-  'uppercase',
-  'zIndex',
-  'minSize',
-  'maxSize',
-  'boxShadow',
-  'textShadow',
-  'fontSize',
-  'letterSpacing',
-  'fontWeight',
-  'fontFamily',
-  'lineHeight',
-  'css',
-  'disableFocusStyles',
-  '_variants',
-  '_selected',
-  'bgGradient',
-  'backgroundGradient',
-  'bgClip',
-  'backgroundClip',
-  'focusBorderColor',
-  'align',
-  'justify',
-  'wrap',
-  'direction',
-  'basis',
-  'grow',
-  'shrink',
-  'spacing',
-  'transform',
-  'transformOrigin',
-  'enableGpuAcceleration',
-  'scale',
-  'scaleX',
-  'scaleY',
-  'rotate',
-  'translateX',
-  'translateY',
-  'skewY',
-  'skewX',
-  'ref',
-  '_invalid',
-  'invalidFocusBorderColor',
-]
-
-const ALLOWED_DOCGEN_NODE_MODULES = ['tippy.js']
-
 // seems there is a current issues with webpack, storybook and Chakra deps
 // https://github.com/chakra-ui/chakra-ui/issues/2527#issuecomment-810293915
 module.exports = {
-  core: {
-    builder: 'webpack5',
-  },
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
   refs: {
@@ -68,7 +11,7 @@ module.exports = {
       disable: true,
     },
   },
-  webpackFinal: async config => {
+  webpackFinal: async (config) => {
     return {
       ...config,
       resolve: {
@@ -80,37 +23,5 @@ module.exports = {
         },
       },
     };
-  },
-  typescript: {
-    check: true,
-    reactDocgen: 'react-docgen-typescript',
-    reactDocgenTypescriptOptions: {
-      shouldExtractLiteralValuesFromEnum: true,
-      propFilter: (prop, component) => {
-        if (component.name === 'Box') {
-          return false
-        }
-        if (EXLUDED_DOCGEN_PROPS.includes(prop.name)) {
-          return false
-        }
-        if (
-          prop.parent &&
-          prop.parent.fileName &&
-          ALLOWED_DOCGEN_NODE_MODULES.some(module => prop.parent.fileName.includes(module))
-        ) {
-          return true
-        }
-        if (prop.parent && /node_modules/.test(prop.parent.fileName)) {
-          return false
-        }
-
-        return true
-      },
-
-      compilerOptions: {
-        allowSyntheticDefaultImports: false,
-        esModuleInterop: false,
-      },
-    },
   },
 }

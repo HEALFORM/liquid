0f03f0a7cb1a77f4a9e369e1079afeb87c165faa
Domenik Töfflinger <domenikt96@yahoo.de>
2022-02-17 03:05:22 +0100

    feat(Body): Adds new additional styles for dark mode.

--- src/components/Body/Body.theme.ts
+++ src/components/Body/Body.theme.ts
@@ -26,14 +26,14 @@ export const Body = {
       borderStyle: 'solid',
       borderColor: tokens.colors.primary.value,
     },
-    success: {
-      color: tokens.colors.success.value,
-    },
-    error: {
-      color: tokens.colors.danger.value,
-    },
-    subtle: {
-      color: tokens.colors.gray['500'].value,
-    },
+    success: ({ colorMode }) => ({
+      color: colorMode === 'dark' ? 'green.300' : 'green.500',
+    }),
+    error: ({ colorMode }) => ({
+      color: colorMode === 'dark' ? 'red.300' : 'red.500',
+    }),
+    subtle: ({ colorMode }) => ({
+      color: colorMode === 'dark' ? 'gray.500' : 'gray.400',
+    }),
   },
 }

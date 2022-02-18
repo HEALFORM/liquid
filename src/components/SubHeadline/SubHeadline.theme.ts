05a972ba9b9eda1daa09f1265de7a318024c96e3
Domenik Töfflinger <domenikt96@yahoo.de>
2022-02-18 16:14:19 +0100

    feat(SubHeadline): Add SubHeadline component with props.

---
+++ src/components/SubHeadline/SubHeadline.theme.ts
@@ -0,0 +1,11 @@
+import tokens from '@healform/design-tokens/dist/js/tokens'
+
+export const SubHeadline = {
+  baseStyle: {
+    textTransform: 'uppercase',
+    fontWeight: tokens.fonts.fontWeight.bold.value,
+    fontSize: tokens.typography.subHeadline.fontSize.value,
+    lineHeight: tokens.typography.subHeadline.lineHeight.value,
+    fontFamily: tokens.fonts.fontStack.special.value,
+  },
+}
---
+++ src/components/SubHeadline/SubHeadline.tsx
@@ -0,0 +1,34 @@
+import { Box, useStyleConfig } from '@chakra-ui/react'
+import tokens from '@healform/design-tokens/dist/js/tokens'
+import React, { HTMLAttributes, ReactNode } from 'react'
+
+export interface SubHeadlineProps extends HTMLAttributes<HTMLHeadingElement> {
+  children: ReactNode
+  /**
+   * Removes the default bottom margin from the headline.
+   */
+  noMargin?: boolean
+  /**
+   * The HTML heading element to render.
+   * Headings should be nested sequentially without skipping any levels.
+   * Learn more at https://www.w3.org/WAI/tutorials/page-structure/headings/.
+   */
+  as: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6'
+}
+
+export const SubHeadline = React.forwardRef<HTMLAttributes<any>, SubHeadlineProps>(function SubHeadline({
+  children,
+  ...props
+}) {
+  const { noMargin, ...rest } = props
+  const styles = useStyleConfig('SubHeadline')
+  return (
+    <Box sx={styles} {...rest} mb={noMargin ? '0' : tokens.spacings.sm.value} as={props.as}>
+      {children}
+    </Box>
+  )
+})
+
+SubHeadline.defaultProps = {
+  as: 'h4',
+}

200ff1f2f4ed087356db5c45f833a79c1fd54d9b
Domenik Töfflinger <domenikt96@yahoo.de>
2022-02-18 14:41:40 +0100

    Include custom IconButton theming in theme/index.ts

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -7,6 +7,7 @@ import { Button } from '../components/Button/Button.theme'
 import { Divider } from '../components/Divider/Divider.theme'
 import { FooterHeadline } from '../components/FooterHeadline/FooterHeadline.theme'
 import { Headline } from '../components/Headline/Headline.theme'
+import { IconButton } from '../components/IconButton/IconButton.theme'
 import { Title } from '../components/Title/Title.theme'
 import * as colors from './colors.theme'
 import * as radii from './radii.theme'
@@ -33,6 +34,7 @@ export default extendTheme({
     Divider,
     FooterHeadline,
     Headline,
+    IconButton,
     Title,
   },
 })

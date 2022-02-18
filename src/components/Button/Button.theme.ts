f9e950d2a06d7a03cc27cd21a5f11853b2bc008d
Domenik Töfflinger <domenikt96@yahoo.de>
2022-02-18 14:39:36 +0100

    Reduce borderRadius in custom button theme.

--- src/components/Button/Button.theme.ts
+++ src/components/Button/Button.theme.ts
@@ -2,7 +2,7 @@ import tokens from '@healform/design-tokens/dist/js/tokens'
 
 export const Button = {
   baseStyle: {
-    borderRadius: tokens.radius.xl.value,
+    borderRadius: tokens.radius.sm.value,
     fontFamily: tokens.fonts.fontStack.special.value,
     fontWeight: tokens.fonts.fontWeight.medium.value,
     _focus: {

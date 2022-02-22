396d6995f45133e7014a76e67d81ad99b0235961
Domenik Töfflinger <domenikt96@yahoo.de>
2022-02-22 18:34:25 +0100

    Create BaseTypographyProps

---
+++ src/interface/Typography.tsx
@@ -0,0 +1,16 @@
+export interface BaseTypographyProps {
+  /**
+   * Removes the default bottom margin from the headline.
+   */
+  noMargin?: boolean
+  /**
+   * if `true`, it'll render an ellipsis when the text exceeds the width of the viewport or maxWidth set.
+   */
+  isTruncated?: boolean
+  /**
+   * The HTML heading element to render.
+   * Headings should be nested sequentially without skipping any levels.
+   * Learn more at https://www.w3.org/WAI/tutorials/page-structure/headings/.
+   */
+  as: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6'
+}

65d6435706cad40f2e6cc7e1e1d5eb8e05d908da
Domenik Töfflinger <domenikt96@yahoo.de>
2022-02-22 16:50:23 +0100

    fix(Button): Change `borderRadius` of ButtonComponent to `xl`

--- src/components/Button/Button.theme.ts
+++ src/components/Button/Button.theme.ts
@@ -2,7 +2,7 @@ import tokens from '@healform/design-tokens/dist/js/tokens'
 
 export const Button = {
   baseStyle: {
-    borderRadius: tokens.radius.sm.value,
+    borderRadius: tokens.radius.xl.value,
     fontFamily: tokens.fonts.fontStack.special.value,
     fontWeight: tokens.fonts.fontWeight.medium.value,
     _focus: {

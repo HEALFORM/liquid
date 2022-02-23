1f52ae6d1ed71da07a176280b8fc455919dd2d7c
Domenik Töfflinger <domenikt96@yahoo.de>
2022-02-23 08:11:55 +0100

    fix(Avatar): Prettier on `Avatar`

--- src/components/Avatar/Avatar.tsx
+++ src/components/Avatar/Avatar.tsx
@@ -1,4 +1,4 @@
-import {Avatar as ChakraAvatar, AvatarProps} from '@chakra-ui/react'
+import { Avatar as ChakraAvatar, AvatarProps } from '@chakra-ui/react'
 import React from 'react'
 
 export const Avatar = ({ children, ...props }: AvatarProps) => {

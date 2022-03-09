28610732fb47f3646d7bf8806eb606af5f15c4d8
Domenik Töfflinger <domenikt96@yahoo.de>
2022-03-09 17:20:50 +0100

    fix(FeatureCard): Remove `transform: scale`

--- src/components/FeatureCard/FeatureCard.theme.ts
+++ src/components/FeatureCard/FeatureCard.theme.ts
@@ -1,6 +1,6 @@
+import { mode } from '@chakra-ui/theme-tools'
 import tokens from '@healform/design-tokens/dist/js/tokens'
 import { darken } from 'polished'
-import { mode } from "@chakra-ui/theme-tools";
 
 const imgUrl = 'https://static.healform.de/__assets/bg-1.svg'
 
@@ -14,13 +14,13 @@ export const FeatureCard = {
     fontWeight: tokens.fonts.fontWeight.semibold.value,
     transition: tokens.animations.slow.value,
     width: '100%',
-    _hover: {
-      transform: 'scale(1.01)',
-    },
   },
   variants: {
-    neutral: (props) => ({
-      background: mode(`url(${imgUrl}) right bottom no-repeat, ${darken(0.03, tokens.colors.gray['50'].value)}`, `url(${imgUrl}) right bottom no-repeat, ${darken(0.03, tokens.colors.gray['900'].value)}`)(props)
+    neutral: props => ({
+      background: mode(
+        `url(${imgUrl}) right bottom no-repeat, ${darken(0.03, tokens.colors.gray['50'].value)}`,
+        `url(${imgUrl}) right bottom no-repeat, ${darken(0.03, tokens.colors.gray['900'].value)}`,
+      )(props),
     }),
     gradientPrimary: {
       background: `url(${imgUrl}) right bottom no-repeat, linear-gradient(80.45deg, ${tokens.colors.primary.value} 0%, ${tokens.colors.primaryDark.value} 99.59%)`,

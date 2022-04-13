492d922dfb8b996fcb7b66fd76639d39ff68c8de
Domenik Töfflinger <domenikt96@yahoo.de>
2022-04-13 16:10:59 +0200

    Refactor `Badge` using updated CSS properties

--- src/components/Badge/Badge.theme.ts
+++ src/components/Badge/Badge.theme.ts
@@ -1,10 +1,8 @@
-import tokens from '@healform/design-tokens/dist/js/tokens'
-
 export const Badge = {
   baseStyle: {
     px: 2,
     py: 0,
     borderRadius: 'xl',
-    fontFamily: tokens.fonts.fontStack.special.value,
+    fontFamily: 'heading',
   },
 }

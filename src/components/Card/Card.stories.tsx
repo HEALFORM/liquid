e2a5b5996a4b897454eee75f0d228fc88dd8cc52
Domenik Töfflinger <domenikt96@yahoo.de>
2022-05-05 18:02:11 +0200

    feat(Card): Adds Card component to the UI library

---
+++ src/components/Card/Card.stories.tsx
@@ -0,0 +1,97 @@
+import React from 'react'
+
+import { Box } from './../../index'
+import { Card, CardProps } from './Card'
+
+export default {
+  title: 'Components/Card',
+  component: Card,
+  argTypes: {
+    image: {
+      name: 'Card Image',
+      control: {
+        type: 'text',
+      },
+    },
+    imageRatio: {
+      name: 'Image Ratio',
+      table: {
+        type: { summary: 'square|rectangle' },
+      },
+      control: {
+        type: 'select',
+        options: ['square', 'rectangle'],
+      },
+    },
+    title: {
+      name: 'Card Title',
+      control: {
+        type: 'text',
+      },
+    },
+    description: {
+      name: 'Card Description',
+      control: {
+        type: 'text',
+      },
+    },
+    url: {
+      name: 'Card URL',
+      control: {
+        type: 'text',
+      },
+    },
+  },
+}
+
+const image = 'https://static.healform.de/__assets/content__facebook/img/og-image_healform-main.jpg'
+const title = 'Bumper Plates'
+const description = 'Bumper plates are a great way to add a little extra protection to your vehicle.'
+const url = 'https://www.healform.de/'
+const buttonText = 'Learn More'
+
+export const Base = (args: CardProps) => (
+  <Box maxW="400px">
+    <Card image={image} title={title} description={description} url={url} buttonText={buttonText} {...args} />
+  </Box>
+)
+
+export const SquareImage = (args: CardProps) => (
+  <Box maxW="400px">
+    <Card
+      image={image}
+      imageRatio="square"
+      title={title}
+      description={description}
+      url={url}
+      buttonText={buttonText}
+      {...args}
+    />
+  </Box>
+)
+
+export const RectangleImage = (args: CardProps) => (
+  <Box maxW="400px">
+    <Card
+      image={image}
+      imageRatio="rectangle"
+      title={title}
+      description={description}
+      url={url}
+      buttonText={buttonText}
+      {...args}
+    />
+  </Box>
+)
+
+export const TextDescriptionOnly = (args: CardProps) => (
+  <Box maxW="400px">
+    <Card title={title} description={description} {...args} />
+  </Box>
+)
+
+export const TextDescriptionButtonOnly = (args: CardProps) => (
+  <Box maxW="400px">
+    <Card title={title} description={description} url={url} buttonText={buttonText} {...args} />
+  </Box>
+)
